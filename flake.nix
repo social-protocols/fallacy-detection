89ef51c8474bf0f042dadccc2e7ee4ff84bbc321
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-08-16 17:47:12 +0200

    use flake instead of devbox (for r and rstudio)

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # for `flake-utils.lib.eachDefaultSystem`
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+          config = {
+            allowUnfree = false;
+            packageOverrides = super: let self = super.pkgs; in
+            {
+              rStudioWithPackages = super.rstudioWrapper.override{
+                packages = with super.rPackages; [
+                  rmarkdown
+                  DBI
+                  RSQLite
+                  lubridate
+                  stringr
+                  dplyr
+                  tidyr
+                  ggplot2
+                  shiny
+                  shinydashboard
+                ];
+              };
+            };
+          };
+        };
+        defaultBuildInputs = with pkgs; [
+          julia-bin
+          sqlite-interactive
+          rStudioWithPackages
+        ];
+      in
+      {
+        devShells.default = with pkgs; pkgs.mkShellNoCC {
+          name = "dev-shell";
+          buildInputs = defaultBuildInputs;
+        };
+        packages.default = with pkgs; pkgs.mkShellNoCC {
+          name = "pack";
+          buildInputs = defaultBuildInputs;
+        };
+      }
+    );
+}
+
